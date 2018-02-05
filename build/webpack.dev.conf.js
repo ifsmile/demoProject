0e073d7e80cf5c9b8719b9ad47c5e2aaca39c01a
weixiaodong <weixiaodongonly@163.com>
2018-02-05 16:25:27 +0800

    delete dir

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,18 +7,12 @@ const path = require('path')
 const baseWebpackConfig = require('./webpack.base.conf')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const portfinder = require('portfinder')
-// const opn = require('opn')
 
-const HOST = process.env.HOST
-const PORT = process.env.PORT && Number(process.env.PORT)
-
-const devWebpackConfig = merge(baseWebpackConfig, {
+module.exports = merge(baseWebpackConfig, {
   devtool: 'cheap-module-eval-source-map',
   module: {
     rules: utils.styleLoaders({ sourceMap: true, usePostCSS: true })
   },
-  // these devServer options should be customized in /config/index.js
   devServer: {
     clientLogLevel: 'warning',
     historyApiFallback: {
@@ -29,7 +23,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     hot: true,
     compress: true,
     host: 'localhost',
-    port: PORT || config.port,
+    port: config.port,
     open: true,
     overlay: { warnings: false, errors: true },
     publicPath: config.localPublicPath,
@@ -54,22 +48,4 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     }),
     new FriendlyErrorsPlugin()
   ]
-})
-module.exports = new Promise((resolve, reject) => {
-  portfinder.basePort = process.env.PORT || config.port
-  portfinder.getPort((err, port) => {
-    const uri = 'http://localhost:' + port
-    if (err) {
-      reject(err)
-    } else {
-      // publish the new Port, necessary for e2e tests
-      process.env.PORT = port
-      // add port to devServer config
-      devWebpackConfig.devServer.port = port
-
-      // opn(uri)
-      console.log(`Listening at ${uri}\n`)
-      resolve(devWebpackConfig)
-    }
-  })
 })
